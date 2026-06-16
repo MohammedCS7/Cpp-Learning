317a7186157babff5b292a249f557f051cfc5188
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-06-16 14:03:02 +0200

    Problem 3 Solved!

---
+++ Problem 1/Problem_3.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+//using namespace std;
+//
+//enum NumberType {Odd = 1 , Even = 2};
+//
+//int ReadNumber()
+//{
+//    int num;
+//    cout << "Please enter a number : " << endl;
+//    cin >> num;
+//    return num;
+//}
+//
+//NumberType CheckNumberType(int num)
+//{
+//    if (num % 2 == 0)
+//    {
+//        return NumberType::Even;
+//    }
+//    else
+//    {
+//        return NumberType::Odd;
+//    }
+//}
+//
+//void PrintNumberType(NumberType numType)
+//{
+//    if (numType == NumberType::Odd)
+//        cout << "\nNumber is Odd. \n ";
+//    else
+//        cout << "\nNumber is Even. \n";
+//     
+//}
+//
+//
+//int main() {
+//    PrintNumberType(CheckNumberType(ReadNumber()));
+//    return 0;
+//}
