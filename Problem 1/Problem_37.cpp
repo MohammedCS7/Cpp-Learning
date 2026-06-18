cc15d4652135aa907b0deec26fd4a232ee6a3af2
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-06-18 14:51:08 +0200

    Problem 37 Solved!

---
+++ Problem 1/Problem_37.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadNumber(string message)
+{
+    int number;
+
+    cout << message << endl;
+    cin >> number;
+
+    return number;
+}
+
+int SumUntilMinus99()
+{
+    int sum = 0;
+    int number = 0;
+    int counter = 1;
+    do
+    {
+        number = ReadNumber("Enter Number : "+to_string(counter));
+        counter++;
+        if(number != -99)
+        sum += number;
+    } while (number != -99);
+    return sum;
+}
+void PrintSum(int sum)
+{
+    cout << "\nThe Sum is : " << sum << endl;
+}
+int main() {
+    int sum = SumUntilMinus99();
+    PrintSum(sum);
+    return 0;
+}
