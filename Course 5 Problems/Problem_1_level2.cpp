d760ff56a0201611ae6b567256467dc5e10168b1
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-06-19 19:07:31 +0200

    Added Course 5 Files

---
+++ Course 5 Problems/Problem_1_level2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+void PrintTableHeader()
+{
+    cout << "\n\n\t\t\tMultiplication Table From 1 to 10" << "\n\n";
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "\t" << i;
+    }
+    cout << "\n-------------------------------------------------------------------------------------------\n";
+}
+string ColumnSeperator(int i)
+{
+    if (i < 10)
+        return "  |";
+    else
+        return " |";
+}
+void PrintMultiplicationTable()
+{
+    
+    PrintTableHeader();
+    for (int i = 1; i <= 10; i++)
+    {
+        
+        cout << " " << i << ColumnSeperator(i) << "\t";
+        for (int j = 1; j <= 10; j++)
+        {
+            cout  << j * i << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintMultiplicationTable();
+
+
+
+    return 0;
+}
