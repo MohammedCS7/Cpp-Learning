a989a92fff3a07538f622042e8a3f72a43da277a
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-07-17 10:07:02 +0200

    Problem 13 Level 4 Solved!

---
+++ Course 8 Problems/Problem_13_level4.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+using namespace std;
+
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+int ReadPositiveNumber(string message)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << message;
+            cin >> num;
+        }
+    } while (num < 0);
+    return num;
+}
+
+int ReadNumberInRange(string message, int from, int to)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+    } while (num < from || num > to);
+    return num;
+}
+
+bool IsLeapYear(int year)
+{
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+int GetMonthDays(int year, int month)
+{
+    int monthsDays[13] = { 0,31,0,31,30,31,30,31,31,30,31,30,31 };
+    return month == 2 ? IsLeapYear(year) ? 29 : 28 : monthsDays[month];
+}
+
+int GetYearDays(int year)
+{
+    return IsLeapYear(year) ? 366 : 365;
+}
+
+void FillArrayWithMonthDays(int year, int monthsDays[12])
+{
+    for (int i = 1; i <= 12; i++)
+    {
+        monthsDays[i - 1] = GetMonthDays(year, i);
+    }
+}
+
+int ReadYear()
+{
+    return ReadPositiveNumber("Please enter a year : ");
+}
+
+int ReadMonth()
+{
+    return ReadNumberInRange("Please enter a month : ", 1, 12);
+}
+
+int ReadDay(int year, int month)
+{
+    return ReadNumberInRange("Please enter a day : ", 1, GetMonthDays(year, month));
+}
+
+Date ReadDate()
+{
+    Date date;
+    date.year = ReadYear();
+    date.month = ReadMonth();
+    date.day = ReadDay(date.year, date.month);
+    return date;
+}
+
+int GetDayOrder(int year, int month, int day)
+{
+    int a = (14 - month) / 12;
+    int y = year - a;
+    int m = month + 12 * a - 2;
+
+    int d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m / 12)) % 7;
+
+    return d;
+}
+
+int GetTotalDays(int year, int month, int days)
+{
+    int total = 0;
+    for (int i = 1; i < month; i++)
+    {
+        total += GetMonthDays(year, i);
+    }
+    return total += days;
+}
+
+
+bool IsDateLower(Date date1, Date date2)
+{
+    return (date1.year != date2.year) ? date1.year < date2.year : GetTotalDays(date1.year, date1.month, date1.day) < GetTotalDays(date2.year, date2.month, date2.day);
+}
+
+bool IsDateLowerTeacherMethod(Date date1, Date date2)
+{
+    return (date1.year != date2.year) ? date1.year < date2.year : date1.month != date2.month ? date1.month < date2.month : date1.day < date2.day;
+}
+
+void PrintDateCompare(bool isLower)
+{
+    if (isLower)
+    {
+        cout << "Yes, Date 1 is fewer than Date 2\n";
+    }
+    else
+    {
+        cout << "No, Date 2 is fewer than Date 1\n";
+    }
+}
+
+
+int main() {
+
+    Date date1 = ReadDate();
+    cout << "\n\n";
+    Date date2 = ReadDate();
+    cout << "\n\n";
+
+    PrintDateCompare(IsDateLower(date1, date2));
+
+    return 0;
+}
