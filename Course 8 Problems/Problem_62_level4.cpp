a25ea06aa4710b061a99db94628076f7f9a9ac15
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-07-20 12:51:21 +0200

    Problem 62 Level 4 Solved!

---
+++ Course 8 Problems/Problem_62_level4.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+enum enDateState { Before = -1, Equal = 0, After = 1 };
+
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct Period
+{
+    Date StartDate;
+    Date EndDate;
+};
+
+int ReadPositiveNumber(string message)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << message;
+            cin >> num;
+        }
+    } while (num < 0);
+    return num;
+}
+
+int ReadNumberInRange(string message, int from, int to)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+    } while (num < from || num > to);
+    return num;
+}
+
+bool IsLeapYear(int year)
+{
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+int GetMonthDays(int year, int month)
+{
+    int monthsDays[13] = { 0,31,0,31,30,31,30,31,31,30,31,30,31 };
+    return month == 2 ? IsLeapYear(year) ? 29 : 28 : monthsDays[month];
+}
+
+int GetYearDays(int year)
+{
+    return IsLeapYear(year) ? 366 : 365;
+}
+
+int ReadYear()
+{
+    return ReadPositiveNumber("Please enter a year : ");
+}
+
+int ReadMonth()
+{
+    return ReadNumberInRange("Please enter a month : ", 1, 12);
+}
+
+int ReadDay(int year, int month)
+{
+    return ReadNumberInRange("Please enter a day : ", 1, GetMonthDays(year, month));
+}
+
+bool IsLastDay(Date date)
+{
+    return (date.day == GetMonthDays(date.year, date.month));
+}
+
+bool IsLastMonth(Date date)
+{
+    return date.month == 12;
+}
+
+Date ReadDate()
+{
+    Date date;
+    date.year = ReadYear();
+    date.month = ReadMonth();
+    date.day = ReadDay(date.year, date.month);
+    cout << "\n\n";
+    return date;
+}
+
+bool IsValidDate(Date date)
+{
+    return (date.year > 0 && date.month <= 12 && date.month > 0 && date.day <= GetMonthDays(date.year, date.month) && date.day > 0);
+}
+
+int main() {
+
+    
+    Date date;
+
+    cout << "Please enter a day : "; cin >> date.day;
+    cout << "\nPlease enter a month : "; cin >> date.month;
+    cout << "\nPlease enter a year : "; cin >> date.year;
+
+    if (IsValidDate(date))
+        cout << "\n\nYes, date is a valid date.\n";
+    else
+        cout << "\n\nNo, date is NOT a valid date. \n";
+
+    return 0;
+}
