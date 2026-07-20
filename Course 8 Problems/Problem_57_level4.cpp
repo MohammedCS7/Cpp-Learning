9ed630c4f2b3cb30d4b9c4da61b29bea87804b20
Mohammed Shihab <mohammedsudan399@gmail.com>
2026-07-20 12:50:28 +0200

    Problem 57 Level 4 Solved!

---
+++ Course 8 Problems/Problem_57_level4.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+enum enDateState { Before = -1, Equal = 0, After = 1 };
+
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+int ReadPositiveNumber(string message)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << message;
+            cin >> num;
+        }
+    } while (num < 0);
+    return num;
+}
+
+int ReadNumberInRange(string message, int from, int to)
+{
+    int num;
+    do
+    {
+        cout << message;
+        cin >> num;
+    } while (num < from || num > to);
+    return num;
+}
+
+bool IsLeapYear(int year)
+{
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+int GetMonthDays(int year, int month)
+{
+    int monthsDays[13] = { 0,31,0,31,30,31,30,31,31,30,31,30,31 };
+    return month == 2 ? IsLeapYear(year) ? 29 : 28 : monthsDays[month];
+}
+
+int GetYearDays(int year)
+{
+    return IsLeapYear(year) ? 366 : 365;
+}
+
+int ReadYear()
+{
+    return ReadPositiveNumber("Please enter a year : ");
+}
+
+int ReadMonth()
+{
+    return ReadNumberInRange("Please enter a month : ", 1, 12);
+}
+
+int ReadDay(int year, int month)
+{
+    return ReadNumberInRange("Please enter a day : ", 1, GetMonthDays(year, month));
+}
+
+int GetTotalDays(Date date)
+{
+    int total = 0;
+    for (int i = 1; i < date.month; i++)
+    {
+        total += GetMonthDays(date.year, i);
+    }
+    return total += date.day;
+}
+
+Date ReadDate()
+{
+    Date date;
+    date.year = ReadYear();
+    date.month = ReadMonth();
+    date.day = ReadDay(date.year, date.month);
+    cout << "\n\n";
+    return date;
+}
+
+bool IsDateLower(const Date& date1,const Date& date2)
+{
+    return (date1.year != date2.year) ? date1.year < date2.year : GetTotalDays(date1) < GetTotalDays(date2);
+}
+
+bool IsDateEqual(const Date& date1,const Date& date2)
+{
+    return (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day);
+}
+
+bool IsDateAfter(const Date& date1,const Date& date2)
+{
+    return !(IsDateLower(date1, date2) || IsDateEqual(date1, date2));
+}
+
+
+short CompareDate(const Date& date1, const Date& date2)
+{
+    return (IsDateLower(date1, date2)) ? -1 : IsDateEqual(date1, date2) ? 0 : 1;
+}
+
+enDateState GetDateCompare(const Date& date1,const Date& date2)
+{
+    return (enDateState)CompareDate(date1, date2);
+}
+
+int main() {
+
+    cout << "Enter Date1:" << endl;
+    Date date1 = ReadDate();
+
+    cout << "\nEnter Date2:" << endl;
+    Date date2 = ReadDate();
+
+    cout << "\nCompare Result = " << GetDateCompare(date1, date2) << endl;
+
+
+    return 0;
+}
